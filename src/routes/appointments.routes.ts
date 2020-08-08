8450c00c08906462e75b3b15e480a9a3b940a3fc
SAS EDUCACAO <wiliam.tae@gmail.com>
2020-08-08 18:02:15 -0300

    create new route to list all appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,8 +4,13 @@ import { startOfHour, parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
@@ -22,7 +27,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is alredy booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, date);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
