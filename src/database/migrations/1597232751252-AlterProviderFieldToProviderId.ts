0c3f49129d7714250c4da87b0af50abb836561b4
SAS EDUCACAO <wiliam.tae@gmail.com>
2020-08-12 08:50:01 -0300

    add user models

---
+++ src/database/migrations/1597232751252-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class AlterProviderFieldToProviderId1597232751252
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
