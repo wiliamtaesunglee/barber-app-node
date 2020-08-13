073246f1467d73292030515ff9723e11343de4d8
Wil.i.am <wiliam.tae@gmail.com>
2020-08-13 10:23:16 -0300

    save avatar and update avatar on database

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
@@ -34,8 +35,20 @@ usersRoute.patch(
   '/avatar',
   ensureAuthenticated,upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true })
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      })
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
 })
 
 export default usersRoute;
