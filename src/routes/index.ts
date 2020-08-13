378271534f26910afe01166331fe2bdf4ecc2190
Wil.i.am <wiliam.tae@gmail.com>
2020-08-13 04:25:54 -0300

    create new user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments.routes';
+import usersRoute from './users.route';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRoute);
 
 export default routes;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import Users from '../models/Users';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<Users> {
+    const usersRepository = getRepository(Users);
+
+    const checkUserExist = await usersRepository.findOne({
+      where: { email },
+    })
+
+    if (checkUserExist) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
