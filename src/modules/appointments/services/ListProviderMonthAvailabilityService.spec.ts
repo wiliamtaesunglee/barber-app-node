f2db39ddacf6ed53f3f33d41d445bd4ff338ffef
Wil.i.am <wiliam.tae@gmail.com>
2020-10-05 05:36:34 -0300

    month availability service

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProvidersMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProvidersMonthAvailability = new ListProviderMonthAvailabilityService(fakeAppointmentsRepository);
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    for (let init = 7; init < 17; init++) {
+      await fakeAppointmentsRepository.create({
+        provider_id: 'user',
+        date: new Date(2020, 4, 20, init, 0, 0)
+      });
+    }
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 11, 0, 0)
+    });
+
+    const availability = await listProvidersMonthAvailability.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 5
+    });
+
+    expect(availability).toEqual(expect.arrayContaining([
+      { day: 19, available: true },
+      { day: 20, available: false },
+      { day: 21, available: true }
+    ]));
+  });
+})
