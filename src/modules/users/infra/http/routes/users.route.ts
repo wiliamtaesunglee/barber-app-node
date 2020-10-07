622bc9b46607fc5b766aa590e582f64f9ea8b296
Wil.i.am <wiliam.tae@gmail.com>
2020-10-07 06:00:29 -0300

    add celebreate as a joi midleware validation

--- src/modules/users/infra/http/routes/users.route.ts
+++ src/modules/users/infra/http/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 
@@ -16,7 +17,16 @@ const userAvatarController = new UserAvatarController();
 const usersRoute = Router();
 const upload = multer(uploadConfig);
 
-usersRoute.post('/', usersController.create);
+usersRoute.post(
+  '/',
+  celebrate({
+    [Segments.BODY]:{
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create);
 
 usersRoute.patch(
   '/avatar',
