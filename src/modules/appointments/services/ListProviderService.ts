1a26301f0ac8dec007744554bc103f6c303363f4
Wil.i.am <wiliam.tae@gmail.com>
2020-10-18 10:58:01 -0300

    cache configurations

--- src/modules/appointments/services/ListProviderService.ts
+++ src/modules/appointments/services/ListProviderService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import Users from '@modules/users/infra/typeorm/entities/Users';
 
@@ -13,14 +14,25 @@ class ListProviderService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
     ) {}
 
   public async execute({
     user_id,
   }: IRequest): Promise<Users[]> {
-    const users = await this.usersRepository.findAllProviders({
-      execpt_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<Users[]>(`providers-list:${user_id}`);
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        execpt_user_id: user_id,
+      });
+    }
+
+    console.log('A query no banco foi feita')
+
+    await this.cacheProvider.save(`providers-list:${user_id}`, users)
 
     return users;
   };
